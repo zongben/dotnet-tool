345dd94c1ec4143d5a71c79019d5377293d45545
ben <ben76020567@gmail.com>
2025-01-25 13:47:39 +0800

    fix: layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-4xl font-bold text-center mb-20 mt-5">Dotnet Tool</h1>
+<h1 class="text-4xl font-bold text-center mb-20 pt-5">Dotnet Tool</h1>
 <div class="flex flex-col items-center">
   <div class="w-5/6">
     <a
