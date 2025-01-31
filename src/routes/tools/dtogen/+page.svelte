eeac5ff04f4321111d4e3669730600b45304fee5
ben <ben76020567@gmail.com>
2025-01-31 20:10:01 +0800

    mod: ui

--- src/routes/tools/dtogen/+page.svelte
+++ src/routes/tools/dtogen/+page.svelte
@@ -1,24 +1,40 @@
-<div class="flex justify-center items-center h-full">
-  <div class="grid md:grid-cols-3 md:gap-x-2 md:gap-y-3 w-2/3">
-    <span class="md:text-end">database:</span>
-    <select class="md:col-span-2 mb-3 md:mb-0">
+<script lang="ts">
+  import trash from "../../../assets/icons/trash.png";
+
+  const spanClass = "md:text-end";
+  const inputClass = "border-b border-b-gray-500 md:col-span-3 mb-3 md:mb-0";
+  const selectClass = "md:col-span-3 mb-3 md:mb-0";
+</script>
+
+<div class="flex justify-center items-center h-full flex-col">
+  <div class="grid md:grid-cols-4 md:gap-x-2 md:gap-y-4 w-2/3 md:pe-14">
+    <span class={spanClass}>recent:</span>
+    <div class="{selectClass} flex items-center">
+      <select class="me-2 flex-1"> </select>
+      <img
+        src={trash}
+        alt="trashcan icon"
+        class="cursor-pointer size-[18px] text-red-500"
+      />
+    </div>
+    <span class={spanClass}>database:</span>
+    <select class={selectClass}>
       <option>mssql</option>
       <option>sqlite</option>
     </select>
-    <span class="md:text-end">saved settings:</span>
-    <select class="md:col-span-2 mb-3 md:mb-0">
-      <option>default</option>
-      <option>custom</option>
-    </select>
-    <span class="md:text-end">name:</span>
-    <input
-      type="text"
-      class="border-b border-b-gray-500 md:col-span-2 mb-3 md:mb-0"
-    />
-    <span class="md:text-end">connection string:</span>
-    <input
-      type="text"
-      class="border-b border-b-gray-500 md:col-span-2 mb-3 md:mb-0"
-    />
+    <span class={spanClass}>host:</span>
+    <input type="text" class={inputClass} />
+    <span class={spanClass}>init db:</span>
+    <input type="text" class={inputClass} />
+    <span class={spanClass}>username:</span>
+    <input type="text" class={inputClass} />
+    <span class={spanClass}>password:</span>
+    <input class={inputClass} />
+  </div>
+  <div class="mt-8">
+    <button
+      class="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-400 cursor-pointer"
+      >Connect</button
+    >
   </div>
 </div>
