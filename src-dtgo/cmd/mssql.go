5a720c1a93d4e41e9707a5ddd42bfb640a5eef3b
Ben <ben76020567@gmail.com>
2025-02-14 16:30:27 +0800

    add: integrate go and rust

---
+++ src-dtgo/cmd/mssql.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var host = ""
+	var initDB = ""
+	var username = ""
+	var password = ""
+
+	var mssqlCmd = &cobra.Command{
+		Use:   "mssql",
+		Short: "",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+      fmt.Println("host:", host)
+      fmt.Println("init-db:", initDB)
+      fmt.Println("username:", username)
+      fmt.Println("password:", password)
+		},
+	}
+	mssqlCmd.Flags().StringVarP(&host, "host", "", "", "")
+	mssqlCmd.Flags().StringVarP(&initDB, "init-db", "", "", "")
+	mssqlCmd.Flags().StringVarP(&username, "username", "", "", "")
+	mssqlCmd.Flags().StringVarP(&password, "password", "", "", "")
+
+	rootCmd.AddCommand(mssqlCmd)
+}
--- src-tauri/src/tools/dtogen.rs
+++ src-tauri/src/tools/dtogen.rs
@@ -1,7 +1,15 @@
 use std::{env, path::PathBuf, process::Command};
 
+#[derive(Debug, serde::Deserialize)]
+pub struct ConnectArgs {
+    host: String,
+    init_db: String,
+    username: String,
+    password: String,
+}
+
 #[tauri::command]
-pub fn connect() -> Result<String, String> {
+pub fn connect(args: ConnectArgs) -> Result<String, String> {
     let exe_dir = env::current_exe()
         .map_err(|e| e.to_string())?
         .parent()
@@ -11,6 +19,11 @@ pub fn connect() -> Result<String, String> {
     let dtgo_path = PathBuf::from(exe_dir).join("dtgo");
 
     let output = Command::new(dtgo_path)
+        .arg("mssql")
+        .arg(format!("--host={}", args.host))
+        .arg(format!("--init-db={}", args.init_db))
+        .arg(format!("--username={}", args.username))
+        .arg(format!("--password={}", args.password))
         .output()
         .map_err(|e| e.to_string())?;
 
